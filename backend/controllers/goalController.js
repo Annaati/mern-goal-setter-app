d964e8389f44265b7df130043c25bc292699d67b
Abdifatah Annaati <66587544+Annaati@users.noreply.github.com>
2022-02-11 23:57:57 +0300

    added express async error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,38 +1,41 @@
+const asyncHandler = require('express-async-handler') 
+
 //@descr Get Goals
 //@route GET /api/goals
 
 const { Error } = require("mongoose")
 
 //@access private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message:'Set Goal'})
-}
+})
 
 //@descr Set Goal
 //@route POST /api/goals
 //@access private
-const setGoal = (req, res) => {
+const setGoal =  asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Plz insert Textfield')
     }
 
     res.status(200).json({message:'Set Goal'})
-}
+})
 
 //@descr Update Goal
 //@route PUT /api/goals/:id
 //@access private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message:`Update Goal ${req.params.id}`})
-}
+})
 
 //@descr Delete Goal
 //@route DELETE /api/goals/:id
 //@access private
-const deleteGoal = (req, res) => {
+const deleteGoal =  asyncHandler(async (req, res) => {
     res.status(200).json({message:`Delete Goal ${req.params.id}`})
-}
+})
+
 
 module.exports = {
     getGoals,
