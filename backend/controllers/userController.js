44502c22e378fcfc82a9f0b39a84feaa7c826669
Abdifatah Annaati <66587544+Annaati@users.noreply.github.com>
2022-02-15 22:56:06 +0300

    Added JWT For Auth Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,7 +40,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id),
         })
     }else{
         res.status(400)
@@ -62,7 +63,8 @@ const loginUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id),
         })
     }else{
         res.status(400)
@@ -79,6 +81,13 @@ const getMe = asyncHandler(async (req, res) => {
     res.json({message: 'Register User'})
 })
 
+
+//JWT
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '10d'
+    })
+}
 
 
 module.exports = {
